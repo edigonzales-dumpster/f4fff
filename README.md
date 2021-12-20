c750aa1e4555f22fcd293836a95ef85cb5b50bbd
Stefan Ziegler <edi.gonzales@gmail.com>
2021-12-20 19:58:07 +0100

    wip

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # f4fff
 
+## Vorbereitung
 ```
 multipass mount /Users/stefan/sources/f4fff foo:/home/ubuntu/sources/f4fff
 ```
@@ -28,7 +29,7 @@ sudo apt-get install postgis
 ```
 (wegen shp2pgsql)
 
-````
+```
 mkdir -m 0777 ~/pgdata-fff
 mkdir --mode=0777 ~/pgdata-fff
 docker run --rm --name pubdb -p 54322:5432 -v ~/pgdata-fff:/var/lib/postgresql/data:delegated -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=pub ghcr.io/baosystems/postgis:13-3.1
@@ -46,5 +47,21 @@ psql -h localhost -p 54322 -d pub -U postgres -W -f fff_partial.sql
 psql -h localhost -p 54322 -d pub -U postgres -W -f fff_full.sql
 ```
 
-todo:
-- Schlussresultat muss wohl wieder auf mm gerundet werden und aus multi single
+
+## Export
+
+```
+java -jar /Users/stefan/apps/ili2pg-4.6.1/ili2pg-4.6.1.jar --dbhost 192.168.64.2 --dbdatabase pub --dbport 54322 --dbusr postgres --dbpwd postgres --strokeArcs --disableValidation --defaultSrsCode 2056 --createEnumTabs --createGeomIdx --models SO_ALW_Fruchtfolgeflaechen_Publikation_20201109 --dbschema fff_out --schemaimport
+```
+
+```
+java -jar /Users/stefan/apps/ili2pg-4.6.1/ili2pg-4.6.1.jar --dbhost 192.168.64.2 --dbdatabase pub --dbport 54322 --dbusr postgres --dbpwd postgres --disableValidation --defaultSrsCode 2056  --models SO_ALW_Fruchtfolgeflaechen_Publikation_20201109 --dbschema fff_out --export fubar.xtf
+
+xmllint --format fubar.xtf -o fubar.xtf
+
+java -jar /Users/stefan/apps/ilivalidator-1.11.12/ilivalidator-1.11.12.jar --log fubar.log fubar.xtf
+```
+
+
+
+
--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
-/*
-CREATE TABLE fff.fruchtfolgeflaechen_mm
+DROP TABLE IF EXISTS fff.fruchtfolgeflaechen_buffer_mm;
+CREATE TABLE fff.fruchtfolgeflaechen_buffer_mm
 AS
 SELECT 
 	gid,
@@ -11,12 +11,13 @@ SELECT
 	anrechenba,
 	area_aren,
 	area_anrec,
-	(ST_Dump(ST_ReducePrecision(geom, 0.001))).geom AS geom
+	(ST_Dump(ST_ReducePrecision(ST_Buffer(geom, 0.02), 0.01))).geom AS geom
 FROM 
-	fff.fruchtfolgeflaechen_partial
+	fff.fruchtfolgeflaechen_full
 ;	
-CREATE INDEX ON "fff"."fruchtfolgeflaechen_mm" USING GIST ("geom")
+CREATE INDEX ON "fff"."fruchtfolgeflaechen_buffer_mm" USING GIST ("geom")
 ;
+
 
 DROP TABLE IF EXISTS fff.fruchtfolgeflaechen_exteriorring;
 CREATE TABLE fff.fruchtfolgeflaechen_exteriorring
@@ -28,18 +29,17 @@ FROM
 	SELECT 
 		(ST_DumpRings(geom)).geom AS geom
 	FROM 
-		fff.fruchtfolgeflaechen_mm
+		fff.fruchtfolgeflaechen_buffer_mm
 ) AS foo
 ;
 CREATE INDEX ON "fff"."fruchtfolgeflaechen_exteriorring" USING GIST ("geom")
 ;
-*/
 
 DROP TABLE IF EXISTS fff.fruchtfolgeflaechen_union;
 CREATE TABLE fff.fruchtfolgeflaechen_union
 AS
 SELECT 
-	ST_Union(geom) AS geom
+	ST_Union(geom, 0.01) AS geom
 FROM 
 	fff.fruchtfolgeflaechen_exteriorring
 ;
@@ -60,3 +60,107 @@ FROM
 CREATE INDEX ON "fff"."polys" USING GIST ("geom")
 ;
 
+DROP TABLE IF EXISTS fff.polys_point;
+CREATE TABLE fff.polys_point 
+AS
+SELECT 
+	geom,
+	ST_PointOnSurface(geom) AS point
+FROM 
+	fff.polys
+;
+CREATE INDEX ON "fff"."polys_point" USING GIST ("geom")
+;
+
+DROP TABLE IF EXISTS fff.polys_attr;
+CREATE TABLE fff.polys_attr
+AS
+SELECT 
+	DISTINCT ON (polys.point) 
+	gid,
+	t_id,
+	bezeichnun,
+	spezialfal,
+	bfs_nr,
+	datenstand,
+	anrechenba,
+	area_aren,
+	area_anrec,
+	polys.geom AS geometrie
+FROM 
+	fff.fruchtfolgeflaechen_full AS fff
+	INNER JOIN fff.polys_point AS polys 
+	ON ST_Intersects(polys.point, fff.geom)
+;
+CREATE INDEX ON "fff"."polys_attr" USING GIST ("geometrie")
+;
+
+DROP TABLE IF EXISTS fff.polys_join;
+CREATE TABLE fff.polys_join
+AS
+SELECT 
+	(ST_Dump(geometrie)).geom AS geometrie,
+	--	ST_Buffer((ST_Dump(geometrie)).geom, -0.001) AS geometrie,
+	fff.t_id,
+	bezeichnun,
+	spezialfal,
+	bfs_nr,
+	datenstand,
+	anrechenba,
+	area_aren,
+	area_anrec
+FROM 
+(
+	SELECT 
+		t_id, ST_RemoveRepeatedPoints(ST_Union(geometrie)) AS geometrie
+	FROM 
+		fff.polys_attr
+	GROUP BY (t_id)
+) AS foo
+LEFT JOIN fff.fruchtfolgeflaechen_full AS fff
+ON fff.t_id = foo.t_id
+;
+CREATE INDEX ON "fff"."polys_join" USING GIST ("geometrie")
+;
+
+
+DELETE FROM fff_out.fruchtfolgeflaeche;
+INSERT INTO fff_out.fruchtfolgeflaeche 
+(
+	geometrie,
+	bezeichnung,
+	spezialfall,
+	bfs_nr,
+	datenstand,
+	anrechenbar,
+	area_aren,
+	area_anrech
+)
+SELECT 
+	geometrie,
+	/*
+	CASE 
+		WHEN bezeichnun ='bedingt geeignet' THEN 'bedingt_geeignet'
+		WHEN bezeichnun = 'geeignete_FFF' THEN 'geeignet'
+		WHEN bezeichnun = 'bedingt_geeignete_FFF' THEN 'bedingt_geeignet'
+		ELSE bezeichnun 
+	END AS bezeichnung,
+	*/
+	bezeichnun AS bezeichnung,
+	/*
+	CASE 
+		WHEN spezialfal = 'reservezone' THEN 'Reservezone'
+		ELSE spezialfal
+	END AS spezialfall,
+	*/
+	spezialfal AS spezialfall,
+	bfs_nr,
+	to_date(datenstand, 'DD.MM.YYYY') AS datenstand,
+	anrechenba AS anrechenbar,
+	ST_Area(geometrie) / 100.0 AS area_aren,
+	(ST_Area(geometrie) / 100.0)*anrechenba AS area_anrech
+FROM 	
+	fff.polys_join AS polys 
+WHERE 
+	ST_Area(geometrie) > 1
+;
